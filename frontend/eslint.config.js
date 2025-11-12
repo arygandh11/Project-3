1bb7f27a0437792d5fa47138ad31105096dc10a2
Aryan <theresatran1824@gmail.com>
2025-11-12 15:20:20 -0600

    Changed more eslint settings

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -20,7 +20,6 @@ export default defineConfig([
       globals: globals.browser,
     },
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       'no-unused-vars': 'off',
     },
